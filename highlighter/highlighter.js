7797ecac63f56546d66931ef513894621e045d00
Matt Schultz <matt@mattschultz.info>
2021-05-23 17:32:48 +0930

    Confirm before highlighting

--- highlighter/highlighter.js
+++ highlighter/highlighter.js
@@ -1,3 +1,10 @@
 require('./TextHighlighter.js')
 
-var hltr = new window.TextHighlighter(document.body);
+var hltr = new window.TextHighlighter(document.body, {
+    onBeforeHighlight: function (range) {
+        return window.confirm('Send to Notion?');
+    },
+    onAfterHighlight: function (range, highlights) {
+        // TODO: Send to Notion
+    },
+});
