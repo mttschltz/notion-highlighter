a6c80bbe679afabfff3f0a3fb72fd211e79165fa
Matt Schultz <matt@mattschultz.info>
2021-05-23 10:57:48 +0930

    Sync options form

--- options/options.js
+++ options/options.js
@@ -1,3 +1,10 @@
-var OptionsSync = require('webext-options-sync');
+// var browser = require("webextension-polyfill");
+import optionsStorage from './options-storage.js';
 
-console.log('optionssync', JSON.stringify(OptionsSync))
+console.log('optionssync', JSON.stringify(optionsStorage))
+
+optionsStorage.syncForm('#options-form');
+
+document.getElementById('test-log-button').addEventListener('click', () => {
+    optionsStorage.getAll().then((options) => console.log(`options=${JSON.stringify(options)}`))
+})
