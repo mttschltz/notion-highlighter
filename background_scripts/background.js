55e3a516589d57160229a7a77ac2bc2ea4f14161
Matt Schultz <matt@mattschultz.info>
2021-05-27 20:17:16 +0930

    Add context menu option

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,1 +1,8 @@
-// var browser = require("webextension-polyfill");
+var browser = require("webextension-polyfill");
+
+browser.contextMenus.create({
+    id: "notion-highlighter-start",
+    title: "Notion Highlighter: Start on page",
+    contexts: ["all"],
+});
+
