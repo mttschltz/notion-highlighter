c10df1d6536bd532b909cea3a5681f54a9fde386
Matt Schultz <matt@mattschultz.info>
2021-05-29 17:37:36 +0930

    Send Notion options to sharer action

--- highlighter/highlighter.js
+++ highlighter/highlighter.js
@@ -1,7 +1,18 @@
+import optionsStorage from '../options/options-storage.js';
 import shareThis from '../lib/share-this/core'
 import * as notionSharer from "../lib/share-this/sharers/notion";
 
-console.log(`loading share-this`)
+notionSharer.action = (event, item, range) => {
+    // event.preventDefault();
+    console.log(`item`, item) // can't traverse this up to popover... only goes to ul for some reason... not in dom yet?
+    console.log(`range`, range) 
+
+    // get credentials each time in case they are updated
+    optionsStorage.getAll().then(({ integrationToken, rootPageID }) => {
+        console.log(`integrationToken, rootPageID=${integrationToken} ${rootPageID}`)
+    })
+
+}
 
 const selectionShare = shareThis({
     sharers: [ notionSharer ],
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'development',
+    mode: 'development', 
+    devtool: 'cheap-module-source-map', // prevent webpack using eval and erroring in Firefox
     entry: {
         background_scripts: "./background_scripts/background.js",
         options: "./options/options.js",
